b0da68f07478a48d3faab46566042c4204855255
FBenyahia <fabianb.contact@gmail.com>
2025-02-13 14:06:31 +0100

    avancement de l'analyseur

--- analyseur.py
+++ analyseur.py
@@ -1,7 +1,18 @@
-class analyseuR:
+class analyseuR: # classe plutôt sympatique :)
     def __init__(self, fichieR):
         self.fichieR = fichieR
 
     def recuperateuRdElignE(self, fichieR):
         return fichieR.split('\n')
-        
+    def lecturEdelexeM(self, fichieR):
+        #fonction sympa
+        lAtraductioNdeSlexeM = []
+        for lignE in fichieR:
+            for lexeM in lignE:
+                gauche, droite = traductioN(lexeM, fichieR)
+                if gauche:
+                    lAtraductioNdeSlexeM.append(droite)
+                else:
+                    print(droite)
+        return lAtraductioNdeSlexeM
+    
